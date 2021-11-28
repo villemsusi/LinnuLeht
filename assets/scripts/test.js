29bd965e94e86226d3f7fa019317f845f0945531
villemsusi <villem.susi@gmail.com>
2021-11-28 12:38:41 +0200

    Menu margins; Scrollbar removed;

---
+++ assets/scripts/test.js
@@ -0,0 +1,23 @@
+const test = {
+    0: [1, "hallvares_test.jpg", "hallvares"],
+    1: [2, "naerukajakas_test.jpg", "naerukajakas"],
+    2: [3, "rasvatihane_test.jpg", "rasvatihane"],
+    3: [4, "sinikael-part_test.jpg", "sinikael-part"],
+    4: [5, "tuvi_test.jpg", "tuvi"],
+}
+
+function checkAnswer() {
+
+}
+
+
+
+function nextQuestion() {
+
+}
+
+
+
+function func() {
+
+}
